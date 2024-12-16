190c2b278a73963639c6dfe131ca8b3effe8798a
heislerlemos <heisler.stlano@gmail.com>
2024-12-16 11:39:43 +0100

    adicionando prompt

--- Main.py
+++ Main.py
@@ -2,30 +2,104 @@
 from art import *
 import os
 from ClassCordaoDeSeguranca import *
+import inquirer
 
 tprint("Controlo de Camaras Lda")
 
-prompt1 = input ("quer adicionar um novo equipamento ? [sim]][no]")
+
+perguntaprompt= [
+        inquirer.List('estado',
+            message ="Quer adicionar equipamentos ?:",
+            choices= ['sim', 'nao'],
+             ),
+            ]
+    
+resposta= inquirer.prompt(perguntaprompt)
+prompt1 = resposta['estado']
+
+
 lista_de_objectos = []
 
 while prompt1 == 'sim':
     print ("Criando o objecto")
-    
+    # Inserir Perimetro
     try:
-        perimetro = int(input("insira  o perímetro do cordão de segurança:"))    
+        perimetro = float(input("insira  o perímetro do cordão de segurança:"))    
     except ValueError:
-        print("Tem que ser um numero inteiro ")    
+        print("Tem que ser um numero decimal  ")    
         
-    topologia = input("insira tipologia do cordão de segurança, que pode ser uma vedação ou um muro:")
-    altura = input("insira a altura da linha de segurança:")
-    estado = input("insira o estado da linha de segurança, que pode ser: bom, mau ou regular:")
-    quantidade = input("insira o número de cabinas pelas quais passa o cordão de segurança:")
-    iluminacao = input("insira a  iluminacaoverdadeiro se estiver aceso, caso contrário, falso:")
-    estadocabinas = input('insira o estado construtivo das cabinas do cordão de segurança, que pode ser bom, mau ou regular:')
+    # Inserir Topologia            
+    perguntatopologia = [
+            inquirer.List('estado',
+                           message ="Insira o estado da linha de segurança :",
+                           choices= ['muro', 'vedação'],
+                         ),
+            ]
+    
+    topologia = inquirer.prompt(perguntatopologia)
+    
+    
+    # Inserir Altura
+    try:
+        altura = float(input("insira a altura da linha de segurança:"))
+    except ValueError:
+        print("Tem que ser decimal")
+    
+    # Inserir Estado
+    
+    perguntalinha = [
+            inquirer.List('estado',
+                           message ="Insira o estado da linha de segurança :",
+                           choices= ['bom', 'mau', 'regular'],
+                         ),
+            ]
+    
+    estado = inquirer.prompt(perguntalinha)
+    
+    # Inserir Quantidade
+    try: 
+        quantidade = int(input("insira o número de cabinas pelas quais passa o cordão de segurança:"))
+    except ValueError:
+        print("tem que ser numero inteiro")
+    
+    # Inserir Iluminação      
+    iluminacao = input("insira a  iluminação  [v] se estiver acceso, caso contrário, [f]:")
+    
+    
+    if iluminacao == 'v':
+        iluminacao = True
+    elif iluminacao == 'f':
+        iluminacao = False
+    else: 
+        print("Valor não aceite")
+        
+    #estadocabinas = input('insira o estado construtivo das cabinas do cordão de segurança, que pode ser bom, mau ou regular:')
+    
+    perguntacabina = [
+            inquirer.List('estadocabina',
+                           message="Insira o estado da linha de segurança :",
+                           choices=['bom', 'mau', 'regular'],
+                         ),
+            ]
+    
+    estadocabinas = inquirer.prompt(perguntacabina)
+    
     
     obj = CordaoDeSeguranca(perimetro,topologia,altura,estado,quantidade,iluminacao,estadocabinas)  
+    
     lista_de_objectos.append(obj)  
-    prompt1 = input ("quer continuar a adicionar novos equipamentos ? [sim][nao]")
+    
+    perguntaprompt= [
+        inquirer.List('estado',
+            message ="Quer adicionar mais  equipamentos ?",
+            choices= ['sim', 'nao'],
+             ),
+            ]
+    
+    resposta = inquirer.prompt(perguntaprompt)
+    prompt1 = resposta['estado']
+
+
 
     os.system('clear')
 
